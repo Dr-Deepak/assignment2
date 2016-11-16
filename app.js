291755ae5777052d775bd96e48c7e511f840d64e
Dr-Deepak <deepak.sharmacpa@gmail.com>
2016-11-15 20:57:47 -0500

    Updated style.css

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,7 +30,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var busInfo = ["ABC Accounting","111-111-1111","1 abc st, Maple ON L6Y 4A2","cs@abcacounting.com","",];
+var pages = ["Home","About","Contact","Clients","Services"];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home',
+                         busName:busInfo[0],
+                         busPh:busInfo[1],
+                         busAdd:busInfo[2],
+                         busEmail:busInfo[3],
+                         busfb:busInfo[4],
+                         bustw:busInfo[5],
+                         pages:pages
+
+                      });
 });
 
 module.exports = router;
